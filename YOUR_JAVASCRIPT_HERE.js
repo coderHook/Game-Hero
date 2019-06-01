8b685e73714a95db58b702e18951a4f9b30ece0d
coderhook <abel1987@hotmail.es>
2019-06-01 12:24:47 +0200

    added Stats showing initial values setted in hero object

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -16,7 +16,6 @@ let dagger = {
     damage: 2
 }
 
-
 /* ----------- F U N C T I O N S --------------*/
 function rest(object) {
     if (object.health === 10) { alert("Your Health was already 10") }
@@ -34,3 +33,19 @@ function equipWeapon(hero) {
     }
 };
 
+//displayStats --> writes your hero's name, health, weapontype, weapon damage to the page.
+function displayStats(hero) {
+    let html = `<div class="stats">
+                    <h3 id="hero-name">${hero.name}</h3>
+                    <p>Health: <span id="health">${hero.health}</span></p>
+                    <p>Weapon: <span id="weapon">${hero.weapon.type}</span></p>
+                    <p>Damage: <span id="damage">${hero.weapon.damage}</span></p>
+                </div>`;
+
+    let body = document.querySelector('body');
+    body.insertAdjacentHTML('afterbegin', html);
+}
+
+
+/* -------------- C A L L I N G  FUNC -------------->   */
+displayStats(hero);
